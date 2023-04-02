7489f70c97a6c2529c0f37303222dc788f3b465c
Elena Yan <elena.yan@studio.unibo.it>
2023-04-02 22:58:39 +0200

    chore: initial example

---
+++ src/java/log/LoggerArch.java
@@ -0,0 +1,28 @@
+package log;
+
+import jason.architecture.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+import jason.runtime.RuntimeServicesFactory;
+
+import java.rmi.RemoteException;
+import java.util.*;
+
+public class LoggerArch extends AgArch {
+
+    // works only with jacamo1.2-SNAPSHOT
+    @Override
+    public void act(ActionExec action) {
+        System.out.println("log: 1.doing "+getTS().getC().getAction().getActionTerm());
+        super.act(action);
+    }
+
+    @Override
+    public void reasoningCycleFinished() {
+        if (getTS().getC().getAction() != null)  {
+            System.out.println("log: 2.doing "+getTS().getC().getAction().getActionTerm());
+        }
+    }
+
+    // TODO: see other methods from AgArch to Override
+}
