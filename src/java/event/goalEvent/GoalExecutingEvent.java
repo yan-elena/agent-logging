8fe12209ac21a68beab2fa4f447b076537be4a62
Elena Yan <elena.yan@studio.unibo.it>
2023-04-15 22:19:49 +0200

    feat(goal): log executing event reason

--- src/java/event/goalEvent/GoalExecutingEvent.java
+++ src/java/event/goalEvent/GoalExecutingEvent.java
@@ -1,8 +1,11 @@
 package event.goalEvent;
 
 import jason.asSemantics.GoalListener;
+import jason.asSyntax.ASSyntax;
+import jason.asSyntax.Structure;
 import jason.asSyntax.Term;
 import jason.asSyntax.Trigger;
+import jason.asSyntax.parser.ParseException;
 
 /**
  * A specific goal event that represents an executed goal event.
@@ -18,5 +21,26 @@ public class GoalExecutingEvent extends GoalEvent {
     public GoalExecutingEvent(int reasoningCycleNum, Trigger goal, Term reason) {
         super(reasoningCycleNum, goal, GoalListener.GoalStates.executing);
         this.getGoalInfo().setReason(reason);
+    }
+
+    @Override
+    public String logEvent() {
+        StringBuilder out = new StringBuilder();
+        out.append("Goal ").append(getGoalInfo().getGoalFunctor()).append(" ").append(getGoalInfo().getGoalStates());
+        if (getGoalInfo().getReason().isPresent()) {
+            try {
+                Term reason = ASSyntax.parseTerm(getGoalInfo().getReason().get());
+                if (reason.isStructure()) {
+                    Structure reasonStruct = ASSyntax.parseStructure(reason.toString());
+                    out.append(" because ").append(reasonStruct.getFunctor().replace("_",
+                            " " + reasonStruct.getTerm(0).toString()  + " is "));
+                } else {
+                    out.append(" with reason: ").append(getGoalInfo().getReason().get());
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        return out.toString();
     }
 }
